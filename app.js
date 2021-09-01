b9a9b3636e975649bd0f2560a01e7818098ad652
sama <samadivinee@gmail.com>
2021-09-01 10:00:28 -0700

    i refactor my code to be able to authenticate with google

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+var findOrCreate = require("mongoose-findorcreate");
 // const bcrypt = require("bcrypt");
 // const saltRounds = 10;
 //const md5 = require("md5");
@@ -48,9 +50,12 @@ const userSchema = new mongoose.Schema({
   email: String,
 
   password: String,
+  googleId: String,
+  secret: String,
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 // on this code i was using the md5 to encryote my password
 // userSchema.plugin(encrypt, {
@@ -61,13 +66,50 @@ const User = new mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      callbackURL: "http://localhost:3000/auth/google/secrets",
+      userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+    },
+    function (accessToken, refreshToken, profile, cb) {
+      User.findOrCreate({ googleId: profile.id }, function (err, user) {
+        return cb(err, user);
+      });
+    }
+  )
+);
 
 app.get("/", (req, res) => {
   res.render("home");
 });
 
+// login with google
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile"] })
+);
+
+app.get(
+  "/auth/google/secrets",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  function (req, res) {
+    // Successful authentication, redirect home.
+    res.redirect("secrets");
+  }
+);
 // login route
 app
   .route("/login")
@@ -111,6 +153,30 @@ app
     });
   });
 
+app.get("/submit", (req, res) => {
+  if (req.isAuthenticated()) {
+    res.render("submit");
+  } else {
+    res.redirect("/login");
+  }
+});
+
+app.post("/submit", (req, res) => {
+  const submitSecret = req.body.secret;
+
+  User.findById(req.user.id, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      if (result) {
+        result.secret = submitSecret;
+        result.save(() => {
+          res.redirect("/secrets");
+        });
+      }
+    }
+  });
+});
 // logout Route
 
 app.get("/logout", function (req, res) {
@@ -124,11 +190,15 @@ app.get("/register", (req, res) => {
 });
 
 app.get("/secrets", (req, res) => {
-  if (req.isAuthenticated()) {
-    res.render("secrets");
-  } else {
-    res.redirect("/login");
-  }
+  User.find({ secret: { $ne: null } }, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      if (result) {
+        res.render("secrets", { userWithSecret: result });
+      }
+    }
+  });
 });
 
 //   on this code i was using bcrypt to encrypt my password
